870bf2a2acb423064e26a59dea043313fb5145d2
d-pratt22 <156544542+d-pratt22@users.noreply.github.com>
2025-10-24 11:48:09 -0700

    Assignment

---
+++ Profiling/Assets/Scripts/SlowCode.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SlowCode : MonoBehaviour
+{
+    private float timeValueRed;
+    private float timeValueGreen;
+    private float timeValueBlue;
+    void Update()
+    {
+        GetComponent<Renderer>().material.color = ColorForTime();
+        DoSomethingElse();
+    }
+    private Color ColorForTime()
+    {
+        timeValueRed += Time.deltaTime;
+        if (timeValueRed > 1f)
+            timeValueRed -= 1f;
+        timeValueGreen += Time.deltaTime * 2f;
+        if (timeValueGreen > 1f)
+            timeValueGreen -= 1f;
+        timeValueBlue += Time.deltaTime * 3f;
+        if (timeValueBlue > 1f)
+            timeValueBlue -= 1f;
+        return new Color(timeValueRed, timeValueGreen, timeValueBlue);
+    }
+    private void DoSomethingElse()
+    {
+    }
+}
